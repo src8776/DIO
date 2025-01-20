224be6b75bdc9d559f8da825265c4ae88c472360
lennard-s <lennardszyperski@gmail.com>
2025-01-19 19:25:29 -0500

    react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import DataTable from './components/DataTable'
-// import AdminDashPage from './pages/AdminDashboard/AdminDashPage'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import LandingPage from './pages/LandingPage/LandingPage'
+import AdminDash from './components/DataTable'
+import Nav from './components/Nav'
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={ <LandingPage/> }/>
+        <Route path="/admin" element ={ <AdminDash/> }/>
+      </Routes>
+    </div>
+
   )
 }
-
-// using this temporarily to display the page/component i'm working on
-// -- Lenny
-// const App = () => {
-//   return <AdminDashPage />;
-// }
 
 export default App

97de2082d22b68562510f62bc1055c520674feb6
Lennard Szyperski <lennardszyperski@gmail.com>
2025-01-23 18:09:13 -0500

    new member page started

---
+++ frontend/src/pages/AddMember/AddMemberPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Container, Paper, Typography } from '@mui/material';
+
+
+function AddMember() {
+
+    return(
+        <Container>
+            <Paper>
+                <Typography elevation={1}>
+                    New Member Form
+                </Typography>
+            </Paper>
+        </Container>
+    );
+}
+
+export default AddMember;

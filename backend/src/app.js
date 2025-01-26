7a722c54bd93f8da677e95b1afe2b3ef67461e7f
Adam Wang <ahw4546@rit.edu>
2025-01-26 02:05:57 -0500

    change backend api to prepend /api/

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 
 // For health check
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.status(200).send('SERVER UP');
 });
 
-app.post('/upload', upload.single('csv_file'), handleFileUpload);
+app.post('/api/upload', upload.single('csv_file'), handleFileUpload);
 
 // TODO: Add more REST endpoints here
 

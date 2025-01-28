a3084854ac34d55e3843104de73cd388e180a978
dannymm104 <dmm4199@rit.edu>
2025-01-28 14:39:23 -0500

    Added cards, now working on formatting

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,69 @@
 import React from "react";
 import { Box, Container, Paper, Typography } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 
-// @Danny I just had to put something here to make the nav situation work.
-//body is the white background, container is next level up, then cards
 function LandingPage() {
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      <Container maxWidth="lg">
+        <Typography sx={{ color: "black", fontSize: "2em" }}>
+          DIO Organizations
+        </Typography>
+        <Box
+          sx={{
+            bgcolor: "gray",
+            height: "100vh",
+            alignContent: "center",
+            placeContent: "center",
+          }}
+        >
+          <Card sx={{ maxWidth: 400, maxHeight: 400 }}>
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: "text.secondary", fontSize: 14 }}
+              >
+                COMS
+              </Typography>
+              <Typography variant="h5" component="div">
+                Computing Organization for Multicultural Students
+              </Typography>
+              <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                Member
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="medium">Home</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ maxWidth: 400, maxHeight: 400 }}>
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: "text.secondary", fontSize: 14 }}
+              >
+                WIC
+              </Typography>
+              <Typography variant="h5" component="div">
+                Women in Computing
+              </Typography>
+              <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                Member
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="medium">Home</Button>
+            </CardActions>
+          </Card>
+        </Box>
       </Container>
-    </React.Fragment>
+    </>
   );
 }
 
-export default LandingPage();
+export default LandingPage;

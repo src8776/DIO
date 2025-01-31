45585351595b797ccd4721ae3eb58f81dc415aeb
dannymm104 <dmm4199@rit.edu>
2025-01-31 16:22:32 -0500

    Cards displaying, just need to apply the correct css to make it look pretty

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -5,43 +5,78 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import { CardMedia } from "@mui/material";
 
 function LandingPage() {
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="lg">
+      <Container sx={{ marginBottom: "100px" }}>
         <Typography sx={{ color: "black", fontSize: "2em" }}>
           DIO Organizations
         </Typography>
         <Box
           sx={{
-            bgcolor: "gray",
-            height: "100vh",
-            alignContent: "center",
-            placeContent: "center",
+            minWidth: 400,
+            minHeight: 200,
+
+            display: "inline-block",
           }}
         >
-          <Card sx={{ maxWidth: 400, maxHeight: 400 }}>
+          <Card sx={{}}>
+            <CardMedia
+              component="img"
+              sx={{
+                width: "100%",
+                height: 140,
+                objectFit: "contain",
+              }}
+              image="/COMS.png"
+              title="COMS"
+            />
             <CardContent>
               <Typography
                 gutterBottom
-                sx={{ color: "text.secondary", fontSize: 14 }}
+                sx={{
+                  color: "text.secondary",
+                  fontSize: 14,
+                }}
               >
                 COMS
               </Typography>
-              <Typography variant="h5" component="div">
-                Computing Organization for Multicultural Students
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{
+                  maxWidth: "100%",
+                  wordBreak: "break-word",
+                  whiteSpace: "normal",
+                }}
+              >
+                Computing Organization of Multicultural Students
               </Typography>
               <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-                Member
+                Make this one Member
               </Typography>
             </CardContent>
             <CardActions>
               <Button size="medium">Home</Button>
             </CardActions>
           </Card>
-          <Card sx={{ maxWidth: 400, maxHeight: 400 }}>
+        </Box>
+        <Box
+          sx={{
+            display: "inline-block",
+            minWidth: 400,
+            minHeight: 200,
+          }}
+        >
+          <Card sx={{ marginLeft: "50px" }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="/Gritty_resized.jpg"
+              title="WIC"
+            />
             <CardContent>
               <Typography
                 gutterBottom
@@ -49,11 +84,12 @@ function LandingPage() {
               >
                 WIC
               </Typography>
+
               <Typography variant="h5" component="div">
                 Women in Computing
               </Typography>
               <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-                Member
+                Make this one admin
               </Typography>
             </CardContent>
             <CardActions>

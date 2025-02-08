6be21da7f2e3bdd3086d1ac0a083954f349a4b83
Ethan Butts <edb2875@g.rit.edu>
2025-02-08 15:27:23 -0500

    testing shibboleth headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const cors = require('cors');
 const { upload, handleFileUpload } = require('./upload/handleFileUpload');
-
 const app = express();
+
+
+
+app.use((req, res, next) => {
+    console.log('Shibboleth Headers:', req.headers);  // Log all headers to the console
+    next();
+  });
+  
+  app.get('/test-shibboleth', (req, res) => {
+    // get user data from headers
+    const shibbolethUser = req.headers['uid'];
+    const shibbolethEmail = req.headers['mail'];
+  
+    console.log('Shibboleth User:', shibbolethUser);
+    console.log('Shibboleth Email:', shibbolethEmail);
+  
+    // Send to the frontend
+    res.json({
+      user: shibbolethUser,
+      email: shibbolethEmail,
+    });
+  });
+
+
+
 app.use(cors());
 
 // For health check

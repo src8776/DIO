020a51f8a6219a6764f8384b36b0e5609c2260f4
lennard-s <lennardszyperski@gmail.com>
2025-02-13 21:12:30 -0500

    minor layout fixes

--- frontend/src/components/AdminLayout.jsx
+++ frontend/src/components/AdminLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet, useParams } from 'react-router-dom';
 import NavColumn from './NavColumn';
-import { Box, Container } from '@mui/material';
+import { Container } from '@mui/material';
 
 const AdminLayout = () => {
     const { org } = useParams();
 
     return (
-        <Container sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <NavColumn pageTitle={"Admin Links"} orgType={org} />
+        <Container sx={{ display: 'flex' }}>
+            <NavColumn pageTitle={"Admin Links"} orgType={org} sx={{ display: { xs: "none", sm: "block" } }} />
             <Outlet />
         </Container>
     );

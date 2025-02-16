f6878c520bb7d4f5a6803e440fa471be1d8cbf75
Ethan Butts <edb2875@g.rit.edu>
2025-02-16 16:41:36 -0500

    undo test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,6 @@ import AdminLayout from './components/AdminLayout.jsx';
 import lightTheme from './theme/themeLight.js';
 import darkTheme from './theme/themeDark.js';
 
-function getShib() {
-  const uid = user?.uid ?? 'Not provided';
-  const givenName = user?.givenName ?? 'Not provided';
-  const sn = user?.sn ?? 'Not provided';
-  const mail = user?.mail ?? 'Not provided';
-  return (uid);
-}
-
 const App = () => {
   console.log(getShib());
   // dark/light mode based on system preference

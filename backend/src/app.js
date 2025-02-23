22b0f00ae03ee5d9329292e57b24bf7bcfeb6e5c
Ethan Butts <edb2875@g.rit.edu>
2025-02-23 10:39:31 -0500

    app.js env var

--- backend/src/app.js
+++ backend/src/app.js
@@ -56,108 +56,85 @@ app.use('/api/user', userRoutes);  //points any api/user* calls from frontend to
 
 
 
+//Server only code for SAML
+if (process.env.NODE_ENV === "production") {
+    const session = require('express-session')
+    const path = require("path");
+    const { defaultSamlStrategy, SP_CERT } = require('./saml.js')
+    const passport = require('passport')
 
+    const SITE_ROOT = '/saml2'
 
+    app.set('view engine', 'ejs')
+    app.set('views', path.join(__dirname, '/views'));
 
-// const session = require('express-session')
-// const path = require("path");
-// const { defaultSamlStrategy, SP_CERT } = require('./saml.js')
-// const passport = require('passport')
+    const siteRoot = express.Router()
+    app.use(SITE_ROOT, siteRoot)
+    app.set('trust proxy', true);
 
-// const SITE_ROOT = '/saml2'
+    siteRoot.use(session({
+        name: 'express-sess',
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+        store: session.MemoryStore()
+    }))
 
-// // Set express to use the ejs template engine
-// app.set('view engine', 'ejs')
-// app.set('views', path.join(__dirname, '/views'));
+    siteRoot.use(passport.session())
+    passport.use('saml', defaultSamlStrategy)
 
-// // Create a router so all the urls are prefixed by '/nodejs'
-// const siteRoot = express.Router()
-// app.use(SITE_ROOT, siteRoot)
-// // This app is served behind a proxy, so we need express to trust it
-// app.set('trust proxy', true);
+    /* user session */
+    passport.serializeUser(function (user, done) {
+        console.log(user);
+        done(null, user);
+    });
 
-// // Create a session for express and passport to share
-// siteRoot.use(session({
-//     name: 'express-sess',
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: true },
-//     store: session.MemoryStore()
-// }))
+    passport.deserializeUser(function (user, done) {
+        console.log(user);
+        done(null, user);
+    });
+    /* end user session */
 
-// // Apply middleware to the router
-// siteRoot.use(passport.session())
-// // Make passport aware of the saml strategy
-// passport.use('saml', defaultSamlStrategy)
+    siteRoot.get('/', (req, res) => {
+        res.render("app", {
+            user: req.user
+        })
+    })
 
-// /* user session */
-// // Necessary to tell passport how to serialize the user
-// // In a production environment we may just serialize the
-// // user.id and then read from the database when deserializing
-// passport.serializeUser(function (user, done) {
-//     console.log(user);
-//     done(null, user);
-// });
+    /* login example */
+    siteRoot.get('/login', passport.authenticate('saml'))
+    /* end login example */
 
-// // Same as the above, we could just have an id and need to hydrate
-// // that into a full user object. In this example we just store the
-// // full attribute array in the session and retrieve it every time.
-// passport.deserializeUser(function (user, done) {
-//     console.log(user);
-//     done(null, user);
-// });
-// /* end user session */
+    /* acs example */
+    siteRoot.post(
+        "/acs",
+        bodyParser.urlencoded({ extended: false }),
+        passport.authenticate("saml", {
+            failureRedirect: SITE_ROOT,
+            failureFlash: true,
+        }),
+        function (req, res) {
+            res.redirect(SITE_ROOT);
+        },
+    )
+    /* end acs example */
 
-// siteRoot.get('/', (req, res) => {
-//     res.render("app", {
-//         user: req.user
-//     })
-// })
+    /* metadata example */
+    siteRoot.get(
+        "/metadata",
+        (req, res) => {
+            res.set('Content-Type', 'text/xml');
+            res.send(defaultSamlStrategy.generateServiceProviderMetadata(SP_CERT, SP_CERT))
+        }
+    )
+    /* end metadata example */
 
-// /* login example */
-// // Passes the SAML login function handler to passport. 
-// // Passport will then redirect the client to the IdP
-// siteRoot.get('/login', passport.authenticate('saml'))
-// /* end login example */
-
-// /* acs example */
-// // Passes the ACS function to passport. 
-// // Passport will then extract the attributes from the IdP
-// // assertion and store the user in the session.
-// siteRoot.post(
-//     "/acs",
-//     bodyParser.urlencoded({ extended: false }),
-//     passport.authenticate("saml", {
-//         failureRedirect: SITE_ROOT,
-//         failureFlash: true,
-//     }),
-//     function (req, res) {
-//         res.redirect(SITE_ROOT);
-//     },
-// )
-// /* end acs example */
-
-// /* metadata example */
-// siteRoot.get(
-//     "/metadata",
-//     (req, res) => {
-//         res.set('Content-Type', 'text/xml');
-//         res.send(defaultSamlStrategy.generateServiceProviderMetadata(SP_CERT, SP_CERT))
-//     }
-// )
-// /* end metadata example */
-
-// // Destroys the session which clears the user
-// siteRoot.get('/logout', (req, res) => {
-//     req.session.destroy()
-//     res.redirect(SITE_ROOT)
-// })
-
-
-
-
-
+    siteRoot.get('/logout', (req, res) => {
+        req.session.destroy()
+        res.redirect(SITE_ROOT)
+    })
+}
 
 
 

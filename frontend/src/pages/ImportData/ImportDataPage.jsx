c0c546c56b973461870a8404a107bd429d745aa5
lennard-s <lennardszyperski@gmail.com>
2025-02-28 19:39:34 -0500

    styling

--- frontend/src/pages/ImportData/ImportDataPage.jsx
+++ frontend/src/pages/ImportData/ImportDataPage.jsx
@@ -6,7 +6,8 @@ import {
     InputLabel, MenuItem, Paper,
     Select, Table, TableContainer,
     TableHead, TableBody, TableCell,
-    TableRow, TextField, Typography
+    TableRow, TextField, Typography,
+    Skeleton
 } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -142,8 +143,8 @@ export default function ImportDataPage({ onUploadSuccess, onClose }) {
                 </Box>
 
                 {/* Form Elements */}
-                <Box component={"form"} sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 2 }}>
-                    <FormControl required fullWidth>
+                <Box component={"form"} sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <FormControl required fullWidth sx={{ mb: 4 }}>
                         <InputLabel id="event-type-select-label">Event Type</InputLabel>
                         <Select
                             labelId="event-type-select-label"
@@ -161,92 +162,104 @@ export default function ImportDataPage({ onUploadSuccess, onClose }) {
                         </Select>
                     </FormControl>
 
+
                     {/* Conditionally render Volunteer Hours input */}
                     {eventType === "Volunteer Event" && (
-                        <Container sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                            <FormControl required fullWidth>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-                                        label="Event Date"
-                                        id="event-date-select"
-                                        value={eventDate}
-                                        onChange={handleDateChange}
-                                        sx={{ flex: 1 }}
-                                    />
-                                </LocalizationProvider>
-                            </FormControl>
+                        <Container disableGutters sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, justifyContent: 'space-evenly' }}>
+                                <FormControl required sx={{ flex: 1 }} >
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DatePicker
+                                            label="Event Date"
+                                            id="event-date-select"
+                                            value={eventDate}
+                                            onChange={handleDateChange}
+                                        />
+                                    </LocalizationProvider>
+                                </FormControl>
 
-                            <FormControl required fullWidth>
-                                <InputLabel id="volunteer-hours-select-label">Volunteer Hours</InputLabel>
-                                <Select
-                                    labelId="volunteer-hours-select-label"
-                                    id="volunteer-hours-select"
-                                    value={volunteerHours}
-                                    onChange={handleVolunteerHoursChange}
-                                    label="Volunteer Hours"
-                                    sx={{ minWidth: '200px' }}
-                                >
-                                    {[...Array(9)].map((_, index) => (
-                                        <MenuItem key={index} value={index + 1}>
-                                            {index + 1} Hour{index + 1 > 1 ? 's' : ''}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-
-                            <FormControl required fullWidth>
-
-                                {/* Autocomplete for Member Selection */}
-                                <Autocomplete
-                                    options={allMembers}
-                                    getOptionLabel={(option) => option.FullName}
-                                    onChange={(event, value) => {
-                                        // Only add to the list if value is not null or undefined
-                                        if (value) {
-                                            addMemberToList(value);
-                                        }
-                                    }}
-                                    renderInput={(params) => <TextField {...params} label="Add Member" />}
-                                    isOptionEqualToValue={(option, value) => option.MemberID === value.MemberID}
-                                    sx={{ mb: 2 }}
-                                />
-                            </FormControl>
-
-                            <Typography variant="h6">
-                                Selected Members
-                            </Typography>
-                            <TableContainer component={Paper} sx={{ maxHeight: 300, overflow: 'auto' }}>
-                                <Table stickyHeader>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Member</TableCell>
-                                            <TableCell>Date</TableCell>
-                                            <TableCell>Hours</TableCell>
-                                            <TableCell align="center">Remove</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {selectedMembers.map((member) => (
-                                            <TableRow key={member.MemberID} sx={{ }}>
-                                                <TableCell>{member.FullName}</TableCell>
-                                                <TableCell>{dayjs(member.date).format("MM/DD/YYYY")}</TableCell>
-                                                <TableCell>{member.hours}</TableCell>
-                                                <TableCell align="center">
-                                                    <IconButton onClick={() => removeMemberFromList(member.MemberID)}
-                                                        sx={{
-                                                            '&:hover': {
-                                                                color: 'red',
-                                                                backgroundColor: 'rgba(255, 0, 0, 0.1)'
-                                                            },
-                                                        }}>
-                                                        <Remove />
-                                                    </IconButton>
-                                                </TableCell>
-                                            </TableRow>
+                                <FormControl required sx={{ flex: 1 }}>
+                                    <InputLabel id="volunteer-hours-select-label">Volunteer Hours</InputLabel>
+                                    <Select
+                                        labelId="volunteer-hours-select-label"
+                                        id="volunteer-hours-select"
+                                        value={volunteerHours}
+                                        onChange={handleVolunteerHoursChange}
+                                        label="Volunteer Hours"
+                                    >
+                                        {[...Array(9)].map((_, index) => (
+                                            <MenuItem key={index} value={index + 1}>
+                                                {index + 1} Hour{index + 1 > 1 ? 's' : ''}
+                                            </MenuItem>
                                         ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+
+                            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 1, justifyContent: 'space-evenly' }}>
+                                    <Typography variant="h6" sx={{ flex: 1 }}>
+                                        Selected Members
+                                    </Typography>
+                                    <FormControl required fullWidth sx={{ flex: 1 }}>
+                                        {/* Autocomplete for Member Selection */}
+                                        <Autocomplete
+                                            size='small'
+                                            options={allMembers}
+                                            getOptionLabel={(option) => option.FullName}
+                                            onChange={(event, value) => {
+                                                // Only add to the list if value is not null or undefined
+                                                if (value) {
+                                                    addMemberToList(value);
+                                                }
+                                            }}
+                                            renderInput={(params) => <TextField {...params} label="Add Member" />}
+                                            isOptionEqualToValue={(option, value) => option.MemberID === value.MemberID}
+                                        />
+                                    </FormControl>
+
+                                </Box>
+                                <TableContainer component={Paper} sx={{ maxHeight: 300, overflow: 'auto' }}>
+                                    <Table stickyHeader>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Member</TableCell>
+                                                <TableCell>Date</TableCell>
+                                                <TableCell>Hours</TableCell>
+                                                <TableCell align="center">Remove</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {selectedMembers.length === 0 ? (
+                                                <TableRow>
+                                                    <TableCell colSpan={4}>
+                                                        <Skeleton variant="rectangular" height={40} />
+                                                    </TableCell>
+                                                </TableRow>
+                                            ) : (
+                                                selectedMembers.map((member) => (
+                                                    <TableRow key={member.MemberID}>
+                                                        <TableCell>{member.FullName}</TableCell>
+                                                        <TableCell>{dayjs(member.date).format("MM/DD/YYYY")}</TableCell>
+                                                        <TableCell>{member.hours}</TableCell>
+                                                        <TableCell align="center">
+                                                            <IconButton onClick={() => removeMemberFromList(member.MemberID)}
+                                                                sx={{
+                                                                    '&:hover': {
+                                                                        color: 'red',
+                                                                        backgroundColor: 'rgba(255, 0, 0, 0.1)'
+                                                                    },
+                                                                }}>
+                                                                <Remove />
+                                                            </IconButton>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))
+                                            )}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Box>
                         </Container>
                     )}
                 </Box>

7e6e66592d3b1ebb6a52fd004352694f9a36f5fa
lennard-s <lennardszyperski@gmail.com>
2025-03-09 19:14:11 -0400

    removed merge comment

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -5,36 +5,7 @@ import AddClubCard from "./AddClubCard";
 
 // TODO: Role based access control
 // TODO: fetch user's club affiliations (WiC? COMS? Both?)
-/*
-function LandingPage() {
 
-    const [memberID, setMemberID] = useState(0);
-    const [organizationIDs, setOrganizationIDs] = useState([]);
-
-    useEffect(() => {
-        fetch('/api/user/memberID')
-            .then(response => response.json())
-            .then(data => setMemberID(data.memberID))
-            .catch(error => console.error('Error fetching memberID:', error));
-    }, []);
-
-    React.useEffect(() => {
-        fetch(`/api/organizationInfo/organizationIDsByMemberID?memberID=${memberID}`)
-            .then(response => response.json())
-            .then(data => setOrganizationIDs(data))
-            .catch(error => console.error('Error fetching data for MemberName:', error));
-    }, [memberID]);
-
-
-    return (
-        <Container sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, flexWrap: 'wrap', alignItems: 'center', gap: 4, pt: 4 }}>
-            {organizationIDs.map(org => (
-                <ClubCard key={org.OrganizationID} memberID={memberID} orgID={org.OrganizationID} />
-            ))}
-        </Container>
-    );
-}
-*/
 function LandingPage() {
     const [memberID, setMemberID] = useState(null);
     const [organizationIDs, setOrganizationIDs] = useState([]);

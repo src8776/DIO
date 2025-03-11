2f81d4fb0b0301e1ccbafdb963fb056723b787d3
iKCole <ike_laughter@hotmail.com>
2025-03-11 16:57:00 -0400

    move InsertFileInfo inside transaction

--- backend/src/upload/loadCsv.js
+++ backend/src/upload/loadCsv.js
@@ -163,6 +163,9 @@ const processCsv = async (filePath, eventType, organizationID) => {
             }
           }
 
+          // Insert file info into UploadedFilesHistory
+          await insertFileInfo(filePath, connection);
+
           await connection.commit();
           console.log('Transaction committed successfully');
           resolve();

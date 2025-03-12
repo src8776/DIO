a624d91f84bf2dee984c548481074048ef50a796
Adam Wang <ahw4546@rit.edu>
2025-03-12 01:40:41 -0400

    cloting and org summary formattinh working

--- backend/src/routes/reportRoutes.js
+++ backend/src/routes/reportRoutes.js
@@ -8,11 +8,35 @@ router.post('/', async (req, res) => {
     const data = req.body;
     console.log(data);
     //TODO: Retrieve data from database
-    const members = [];
-    generateReport(res, members);
+    const reportDetails = {
+        reportName: "Computing Organization for Multicultural Students (COMS) Report",
+        semesterName: "Spring Semester 2025",
+        orgSummary: {
+            totalMembers: 32,
+            activeMembers: 23,
+            inactiveMembers: 9,
+            totalEvents: 10
+        },
+        clothingSummary: {
+            shirtSizes: {
+                small: 9,
+                medium: 5,
+                large: 9,
+                xlarge: 9
+            },
+            pantSizes: {
+                size6: 9,
+                size8: 5,
+                size16: 9,
+                size24: 9
+            }
+        },
+        members: [],
+    };
+    generateReport(res, reportDetails);
 });
 
-const generateReport = (res, members) => {
+const generateReport = (res, reportDetails) => {
     // Create a new PDF document
     const doc = new PDFDocument({
         margins: { // Set margins in points (1 point = 1/72 inch)
@@ -30,8 +54,8 @@ const generateReport = (res, members) => {
     doc.on('pageAdded', () => {
         doc.image('src/assets/images/RIT_logo.png', { width: 25 });
         doc.font("Helvetica-Bold").fontSize(12).text(' | DIO Member Dashboard', 75, 50);
-        doc.fontSize(12).font("Helvetica").text('Spring Semester 2025', {align: 'right'});
-        doc.fontSize(15).font("Helvetica-Bold").text('Computing Organization for Multicultural Students (COMS) Report', 50, 75, {width: 256});
+        doc.fontSize(12).font("Helvetica").text(reportDetails.semesterName, {align: 'right'});
+        doc.fontSize(15).font("Helvetica-Bold").text(reportDetails.reportName, 50, 75, {width: 307.2});
         doc.moveDown(1);
         doc.fontSize(10).text('Generated on: ' + new Date().toLocaleString(), 50, doc.y);
         doc.moveDown(1);
@@ -41,30 +65,30 @@ const generateReport = (res, members) => {
 
     // Organization Summary
     const orgSummary = [
-        { label: 'Total Number of Members', value: '32' },
-        { label: 'Total Number of Active Members', value: '72%' },
-        { label: 'Total Number of Inactive Members', value: '28%' },
-        { label: 'Total Number of Events Held', value: '10' }
+        { label: 'Total Number of Members', value: reportDetails.orgSummary.totalMembers },
+        { label: 'Percentage of Active Members', value: [Math.round(reportDetails.orgSummary.activeMembers / reportDetails.orgSummary.totalMembers * 100), '%'].join('') },
+        { label: 'Percentage of Inactive Members', value: [Math.round(reportDetails.orgSummary.inactiveMembers / reportDetails.orgSummary.totalMembers * 100), '%'].join('') },
+        { label: 'Total Number of Events Held', value: reportDetails.orgSummary.totalEvents }
     ];
-    drawSummaryTable(doc, 'Organization Summary', orgSummary, 50, doc.y + 10, 200, 100, 20);
+    drawOrgSummaryTable(doc, 'Organization Summary', orgSummary, 50, doc.y + 10, 200, 100, 20);
 
     doc.moveDown(2);
 
     // Clothing Size Summary
     const clothingSummary = [
-        { label: 'Shirt Size', value: 'Quantity (Members)' },
-        { label: 'Small', value: '9' },
-        { label: 'Medium', value: '5' },
-        { label: 'Large', value: '9' },
-        { label: 'X-Large', value: '9' },
+        { label: 'Shirt Size', value: 'Quantity (Members)', type: 'header' },
+        { label: 'Small', value: reportDetails.clothingSummary.shirtSizes.small },
+        { label: 'Medium', value: reportDetails.clothingSummary.shirtSizes.medium },
+        { label: 'Large', value: reportDetails.clothingSummary.shirtSizes.large },
+        { label: 'X-Large', value: reportDetails.clothingSummary.shirtSizes.xlarge },
         { label: '', value: '' },
-        { label: 'Pant Size', value: 'Quantity (Members)' },
-        { label: '6', value: '9' },
-        { label: '8', value: '5' },
-        { label: '16', value: '9' },
-        { label: '24', value: '9' }
+        { label: 'Pants Size', value: 'Quantity (Members)', type: 'header' },
+        { label: 'Size 6', value: reportDetails.clothingSummary.pantSizes.size6 },
+        { label: 'Size 8', value: reportDetails.clothingSummary.pantSizes.size8 },
+        { label: 'Size 16', value: reportDetails.clothingSummary.pantSizes.size16 },
+        { label: 'Size 24', value: reportDetails.clothingSummary.pantSizes.size24 }
     ];
-    drawSummaryTable(doc, 'Clothing Size Summary', clothingSummary, 50, doc.y + 10, 200, 100, 20);
+    drawClothingSummaryTable(doc, 'Clothing Size Summary', clothingSummary, 50, doc.y + 10, 200, 100, 20);
 
     doc.moveDown(2);
 
@@ -84,7 +108,7 @@ const generateReport = (res, members) => {
     doc.end();
 }
 
-const drawSummaryTable = (doc, title, data, startX, startY, col1Width, col2Width, rowHeight) => {
+const drawOrgSummaryTable = (doc, title, data, startX, startY, col1Width, col2Width, rowHeight) => {
     // Draw table title
     doc.fillColor('#0086A9').fontSize(15).font("Helvetica-Bold").text(title, startX, startY);
     startY += rowHeight / 2; // Adjust startY to account for the title
@@ -94,8 +118,28 @@ const drawSummaryTable = (doc, title, data, startX, startY, col1Width, col2Width
     data.forEach((item, index) => {
         const y = startY + rowHeight * (index + 1);
         doc.text(item.label, startX, y);
-        doc.text(item.value, startX + col1Width + col2Width - item.value.length * 12, y);
-        console.log(item.value.length);
+        let xlocal = startX + col1Width + col2Width - 30;
+        doc.text(item.value, xlocal, y);
+
+        // Draw line beneath the row
+        doc.strokeColor('#CCCCCC') // Set stroke color to gray
+        .moveTo(startX, y + rowHeight - 5)
+        .lineTo(startX + col1Width + col2Width, y + rowHeight - 5)
+        .stroke();
+    });
+}
+
+const drawClothingSummaryTable = (doc, title, data, startX, startY, col1Width, col2Width, rowHeight) => {
+    // Draw table title
+    doc.fillColor('#0086A9').fontSize(15).font("Helvetica-Bold").text(title, startX, startY);
+    startY += rowHeight / 2; // Adjust startY to account for the title
+
+    // Draw table rows
+    doc.fillColor('#000000').fontSize(12).font("Helvetica");
+    data.forEach((item, index) => {
+        const y = startY + rowHeight * (index + 1);
+        doc.text(item.label, startX, y);
+        doc.text(item.value, startX + col1Width, y);
 
         // Draw line beneath the row
         if(item.value !== '' && item.label !== '') {

8b0e54e7cae94114854e18c9138489e14ad1f6aa
lennard-s <lennardszyperski@gmail.com>
2025-03-12 12:28:27 -0400

    link setup

---
+++ frontend/src/pages/AnalyticsDashboard/AnalyticsDashPage.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import * as React from 'react';
+
+
+
+
+export default function AnalyticsDash() {
+
+    return(
+        <Typography>Bing bong</Typography>
+    );
+}

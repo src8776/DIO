0ea8a453394c493c9b71f82b6cfa55aa2b57f07c
lennard-s <lennardszyperski@gmail.com>
2025-03-19 19:15:38 -0400

    can now add attendance records to individual members

--- frontend/src/pages/MemberDetails/MemberDetailsPage.jsx
+++ frontend/src/pages/MemberDetails/MemberDetailsPage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import * as React from 'react';
 import {
   Container, Typography, Table,
   TableBody, TableCell, TableContainer,
   TableHead, TableRow, Paper, Box,
-  Chip,
+  Chip, Button, TextField, Dialog,
+  DialogActions, DialogContent, DialogTitle, Select,
+  MenuItem, InputLabel, FormControl, Drawer
 } from "@mui/material";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import EditIcon from '@mui/icons-material/Edit';
 
 // TODO: Option to delete attendance records from the attendance history table here if they are an admin
 //       - user feedback for deleting records ('this action cannot be undone', 'deleted successfully', etc.)?
@@ -35,6 +42,38 @@ const StatusChip = ({ memberStatus, ...props }) => (
 
 export default function MemberDetailsModal({ memberID, orgID, memberStatus, selectedSemester }) {
   const [memberInfo, setMemberInfo] = React.useState();
+  const [open, setOpen] = React.useState(false);
+  const [eventTypeItems, setEventTypeItems] = React.useState([]);
+  const semesterID = selectedSemester?.SemesterID || null;
+  const semesterStart = selectedSemester?.StartDate || null;
+  const semesterEnd = selectedSemester?.EndDate || null;
+  const [eventDate, setEventDate] = React.useState(() => { //making sure date is in the semester
+    const today = dayjs();
+    if (semesterStart && semesterEnd) {
+      const start = dayjs(semesterStart);
+      const end = dayjs(semesterEnd);
+      if (today.isBefore(start)) {
+        return start;
+      } else if (today.isAfter(end)) {
+        return end;
+      } else {
+        return today;
+      }
+    }
+    return today; // Fallback if semester dates are unavailable
+  }); // eventDate defaults to today's date
+  const [formData, setFormData] = React.useState({
+    memberID: memberID,
+    organizationID: orgID,
+    semesterID: semesterID,
+    eventType: '',
+    eventDate: eventDate,
+    eventTitle: '',
+    attendanceStatus: '',
+    attendanceSource: 'manual adjustment',
+    hours: null
+  });
+
 
   React.useEffect(() => {
     if (!memberID || !orgID) return;
@@ -48,6 +87,57 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
       .catch(error => console.error('Error fetching data for MemberInfo:', error));
   }, [memberID, orgID, selectedSemester]);
 
+  // Only fetch if we have both orgID and semesterID
+  React.useEffect(() => {
+    if (orgID && semesterID) {
+      fetch(`/api/admin/events?organizationID=${orgID}&semesterID=${semesterID}`)
+        .then((response) => response.json())
+        .then((data) => setEventTypeItems(data))
+        .catch((error) => console.error('Error fetching events:', error));
+    }
+  }, [orgID, semesterID]);
+
+  const handleSubmit = () => {
+    const formattedEventDate = formData.eventDate.format('YYYY-MM-DD');
+    fetch('/api/memberDetails/addIndividualAttendance', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ...formData,
+        eventDate: formattedEventDate,
+        hours: formData.eventType === 'Volunteer Event' ? formData.hours : null
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        setOpen(false);
+        fetch(`/api/memberDetails/detailsBySemester?memberID=${memberID}&organizationID=${orgID}&termCode=${selectedSemester.TermCode}`)
+          .then(response => response.json())
+          .then(data => setMemberInfo(data));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleDateChange = (date) => {
+    setFormData({ ...formData, eventDate: date });
+  };
 
   if (!memberInfo || memberInfo.length === 0) {
     return (
@@ -65,7 +155,7 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
 
   const {
     MemberID, UserName, FirstName, LastName, Email,
-    Major, GraduationYear, AcademicYear, attendanceRecords, 
+    Major, GraduationYear, AcademicYear, attendanceRecords,
     RoleName, ShirtSize, PantSize, Gender, Race
   } = memberInfo[0];
 
@@ -74,16 +164,22 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
       <Paper sx={style}>
         <Box sx={{ overflowY: 'auto', p: 2 }}>
           {/* Header Section */}
-          <Box sx={{ mb: 2 }}>
-            <Typography variant="h5" sx={{ fontWeight: 600 }}>
-              {FirstName} {LastName} • <StatusChip
-                label={memberStatus}
-                memberStatus={memberStatus}
-                size="medium"
-              />
-            </Typography>
-            <Typography variant="subtitle2" sx={{ mt: .5 }}>
-              {RoleName} • MemberID: {MemberID}
+          {/* TODO: Make this section sticky */}
+          <Box sx={{ mb: 2, display: 'flex', justifyContent: 'space-between' }}>
+            <Box>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                {FirstName} {LastName} • <StatusChip
+                  label={memberStatus}
+                  memberStatus={memberStatus}
+                  size="medium"
+                />
+              </Typography>
+              <Typography variant="subtitle2" sx={{ mt: .5 }}>
+                {RoleName} • MemberID: {MemberID}
+              </Typography>
+            </Box>
+            <Typography variant="h6">
+              {selectedSemester ? selectedSemester.TermName : "all semesters"}
             </Typography>
           </Box>
 
@@ -135,11 +231,11 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
                 <Typography variant="h6">
                   Attendance History
                 </Typography>
-                <Typography variant="h6">
-                  {selectedSemester ? selectedSemester.TermName : "all semesters"}
-                </Typography>
+                <Button variant="outlined" startIcon={<EditIcon />} onClick={handleClickOpen}>
+                  Edit Attendance
+                </Button>
               </Box>
-              <TableContainer component={Paper} elevation={0} sx={{ maxHeight: 200 }}>
+              <TableContainer component={Paper} elevation={0} sx={{ maxHeight: 300 }}>
                 <Table stickyHeader size="small">
                   <TableHead>
                     <TableRow >
@@ -150,24 +246,26 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
                   </TableHead>
                   <TableBody>
                     {attendanceRecords && attendanceRecords.length > 0 ? (
-                      attendanceRecords.map((record, index) => (
-                        <TableRow key={index} hover>
-                          <TableCell>{record.EventID}</TableCell>
-                          <TableCell>
-                            {record.EventType}
-                            {record.Hours && (
-                              <Chip
-                                label={`${record.Hours}h`}
-                                size="small"
-                                sx={{ ml: 1 }}
-                              />
-                            )}
-                          </TableCell>
-                          <TableCell>
-                            {new Date(record.CheckInTime).toLocaleString()}
-                          </TableCell>
-                        </TableRow>
-                      ))
+                      attendanceRecords
+                        .sort((a, b) => new Date(b.CheckInTime) - new Date(a.CheckInTime))
+                        .map((record, index) => (
+                          <TableRow key={index} hover>
+                            <TableCell>{record.EventID}</TableCell>
+                            <TableCell>
+                              {record.EventType}
+                              {record.Hours && (
+                                <Chip
+                                  label={`${record.Hours}h`}
+                                  size="small"
+                                  sx={{ ml: 1 }}
+                                />
+                              )}
+                            </TableCell>
+                            <TableCell>
+                              {new Date(record.CheckInTime).toLocaleString()}
+                            </TableCell>
+                          </TableRow>
+                        ))
                     ) : (
                       <TableRow>
                         <TableCell colSpan={3} align="center">
@@ -184,6 +282,92 @@ export default function MemberDetailsModal({ memberID, orgID, memberStatus, sele
           </Box>
         </Box>
       </Paper>
+      {/* Add Attendance Dialog */}
+      <Dialog open={open} onClose={handleClose} >
+        <DialogTitle>Add Attendance</DialogTitle>
+        <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 3, overflowY: 'auto', p: 2 }}>
+          {/* Event Type */}
+          <FormControl required fullWidth sx={{ flex: 1 }}>
+            <InputLabel id="event-type-select-label">Event Type</InputLabel>
+            <Select
+              labelId="event-type-select-label"
+              id="event-type-select"
+              name="eventType"
+              value={formData.eventType}
+              onChange={handleChange}
+              label="Event Type"
+              sx={{ minWidth: '200px' }}
+            >
+              {eventTypeItems.map((item) => (
+                <MenuItem key={item.EventTypeID} value={item.EventType}>
+                  {item.EventType}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* Event Title */}
+          <TextField
+            label="Event Title (optional)"
+            name="eventTitle"
+            value={formData.eventTitle}
+            onChange={handleChange}
+            fullWidth
+          />
+          {/* Event Date */}
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Event Date"
+              value={formData.eventDate}
+              onChange={handleDateChange}
+              minDate={dayjs(semesterStart)}
+              maxDate={dayjs(semesterEnd)}
+            />
+          </LocalizationProvider>
+
+          {/* Attendance Status (Excused, Attended) */}
+          <FormControl required fullWidth sx={{ flex: 1 }}>
+            <InputLabel id="attendance-status-select-label">Attendance Status</InputLabel>
+            <Select
+              labelId="attendance-status-select-label"
+              id="attendance-status-select"
+              name="attendanceStatus"
+              value={formData.attendanceStatus}
+              onChange={handleChange}
+              label="Attendance Status"
+            >
+              <MenuItem value="Attended">Attended</MenuItem>
+              <MenuItem value="Excused">Excused</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* Volunteer Hours */}
+          {formData.eventType === 'Volunteer Event' && (
+            <FormControl required sx={{ flex: 1 }}>
+              <InputLabel id="volunteer-hours-select-label">Volunteer Hours</InputLabel>
+              <Select
+                labelId="volunteer-hours-select-label"
+                id="volunteer-hours-select"
+                name="hours"
+                value={formData.hours}
+                onChange={handleChange}
+                label="Volunteer Hours"
+                size="small"
+              >
+                {[...Array(9)].map((_, index) => (
+                  <MenuItem key={index} value={index + 1}>
+                    {index + 1} Hour{index + 1 > 1 ? 's' : ''}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant='outlined'>Cancel</Button>
+          <Button onClick={handleSubmit} variant='contained'>Add</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };

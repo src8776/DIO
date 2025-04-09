f815c893ee5c4d60097f15060a3b2b8e9f62b56b
lennard-s <lennardszyperski@gmail.com>
2025-04-09 18:28:47 -0400

    no alumni check

--- frontend/src/pages/AnalyticsDashboard/GraduatingMembersChart.jsx
+++ frontend/src/pages/AnalyticsDashboard/GraduatingMembersChart.jsx
@@ -144,41 +144,46 @@ export default function GraduatingMembersChart({ organizationID, semesterID: sel
                 <Typography variant="body1" color="text.secondary">No data to display</Typography>
             ) : (
                 <>
-                    <Box sx={{ width: '100%', height: 150 }}>
-                        <ResponsiveContainer>
-                            <PieChart>
-                                <Pie
-                                    data={data}
-                                    dataKey="value"
-                                    nameKey="name"
-                                    cx="50%"
-                                    cy="50%"
-                                    innerRadius={40}
-                                    outerRadius={60}
-                                    fill="#8884d8"
-                                    labelLine={false}
-                                    minAngle={50}
-                                    onClick={(sliceData) => {
-                                        const category = sliceData.name;
-                                        setSelectedCategory(category);
-                                        fetchMembersList(category);
-                                        setDrawerOpen(true);
-                                    }}
-                                >
-                                    {/* <Label value={`${totalMembers ? ((gradCount / totalMembers) * 100).toFixed(1) : 0}%`} position="center" /> */}
-                                    <Label value={showAlumni ? `${alumniCount}\nAlumni` : `${gradCount} / ${totalMembers}`} position="center" />
-                                    {data.map((entry, index) => (
-                                        <Cell
-                                            key={`cell-${index}`}
-                                            fill={entry.name === 'Graduating' || entry.name === 'Alumni' ? 'rgb(75, 153, 105)' : '#8884d8'}
-                                            style={{ cursor: 'pointer' }}
-                                        />
-                                    ))}
-                                </Pie>
-                                {!showAlumni && <Tooltip />}
-                            </PieChart>
-                        </ResponsiveContainer>
-                    </Box>
+                    {showAlumni && !alumniCount ? (
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 150 }}>
+                            <Typography variant="body1" color="text.secondary">No Alumni</Typography>
+                        </Box>
+                    ) : (
+                        <Box sx={{ width: '100%', height: 150 }}>
+                            <ResponsiveContainer>
+                                <PieChart>
+                                    <Pie
+                                        data={data}
+                                        dataKey="value"
+                                        nameKey="name"
+                                        cx="50%"
+                                        cy="50%"
+                                        innerRadius={40}
+                                        outerRadius={60}
+                                        fill="#8884d8"
+                                        labelLine={false}
+                                        minAngle={50}
+                                        onClick={(sliceData) => {
+                                            const category = sliceData.name;
+                                            setSelectedCategory(category);
+                                            fetchMembersList(category);
+                                            setDrawerOpen(true);
+                                        }}
+                                    >
+                                        <Label value={showAlumni ? `${alumniCount}\nAlumni` : `${gradCount} / ${totalMembers}`} position="center" />
+                                        {data.map((entry, index) => (
+                                            <Cell
+                                                key={`cell-${index}`}
+                                                fill={entry.name === 'Graduating' || entry.name === 'Alumni' ? 'rgb(75, 153, 105)' : '#8884d8'}
+                                                style={{ cursor: 'pointer' }}
+                                            />
+                                        ))}
+                                    </Pie>
+                                    {!showAlumni && <Tooltip />}
+                                </PieChart>
+                            </ResponsiveContainer>
+                        </Box>
+                    )}
                 </>
             )}
             <NestedDrawers
